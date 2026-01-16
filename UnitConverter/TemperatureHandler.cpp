ad61006c68dd97d71bb0c77e2e611ce25730e0e6
liam.karv <karvelasliam@gmail.com>
2026-01-16 16:53:52 +1100

    Refactoring to use unordered_map for temp conversions

--- UnitConverter/TemperatureHandler.cpp
+++ UnitConverter/TemperatureHandler.cpp
@@ -1,12 +1,50 @@
 #include <iostream>;
+#include <unordered_map>;
 #include "TemperatureFunctions.h";
+
+using namespace std;
+
+inline double calculateConversion(char fromUnit, char toUnit, double convNumber)
+{
+
+    unordered_map<std::pair<char,char>, std::function>;
+    double result;
+    std::cout << "Calculating..." << std::endl;
+    //switch (fromUnit )
+    //{
+    //    case ('C'):
+    //        switch()
+    //        case ('F'):
+    //            result = convertCtoF(convNumber);
+    //            break;
+
+    //    case ('C' && 'K'):
+    //        result = convertCtoK(convNumber);
+    //        break;
+
+    //    default:
+    //        0;
+    //}
+    //return result;
+}
+
 
 inline void displayTemperatureMenu()
 {
-    std::cout << "0. Close program" << std::endl;
-    std::cout << "1. Go back to main menu" << std::endl;
-    std::cout << "2. Select unit to convert FROM" << std::endl;
-    std::cout << "3. Select unit to convert TO" << std::endl;
+    char fromUnit;
+    char toUnit;
+    double convNumber;
+    std::cout << "Select unit to convert FROM " << std::endl;
+    std::cin >> fromUnit;
+
+    std::cout << "Select unit to convert TO " << std::endl;
+    std::cin >> toUnit;
+
+    std::cout << "Enter a number " << std::endl;
+    std::cin >> convNumber;
+
+    std::cout << calculateConversion(fromUnit, toUnit, convNumber) << std::endl;
+
     return;
 }
 
